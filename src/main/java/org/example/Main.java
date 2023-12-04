ccaeb1bc3e15caef9a5f1806281683132928dde9
SashaPerepelkin <apocaliptorus@gmail.com>
2023-12-05 01:29:06 +0300

    f 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("f - 1");
         System.out.println("f - 2");
+        System.out.println("Gooo");
     }
 }
